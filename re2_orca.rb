0fa878fba8e608afdecb2ad68040cb34237f3ab4
Nick Yasnogorodskyi <nikyasnogorodskyi@gmail.com>
2023-11-19 22:49:16 +0100

    feat: re2 fixed version for orca

---
+++ re2_orca.rb
@@ -0,0 +1,54 @@
+class Re2AT20221201 < Formula
+  desc "Alternative to backtracking PCRE-style regular expression engines"
+  homepage "https://github.com/google/re2"
+  url "https://github.com/google/re2/archive/refs/tags/2022-12-01.tar.gz"
+  version "20221201"
+  sha256 "665b65b6668156db2b46dddd33405cd422bd611352c5052ab3dae6a5fbac5506"
+  license "BSD-3-Clause"
+  head "https://github.com/google/re2.git", branch: "main"
+
+  # The `strategy` block below is used to massage upstream tags into the
+  # YYYYMMDD format used in the `version`. This is necessary for livecheck
+  # to be able to do proper `Version` comparison.
+  livecheck do
+    url :stable
+    regex(/^(\d{2,4}-\d{2}-\d{2})$/i)
+    strategy :git do |tags, regex|
+      tags.map { |tag| tag[regex, 1]&.gsub(/\D/, "") }.compact
+    end
+  end
+
+  depends_on "cmake" => :build
+
+  def install
+    ENV.cxx11
+
+    # Run this for pkg-config files
+    system "make", "common-install", "prefix=#{prefix}"
+
+    # Build and install static library
+    system "cmake", "-B", "build-static", "-DRE2_BUILD_TESTING=OFF", *std_cmake_args
+    system "make", "-C", "build-static"
+    system "make", "-C", "build-static",  "install"
+
+    # Build and install shared library
+    system "cmake", "-B", "build-shared", "-DBUILD_SHARED_LIBS=ON", "-DRE2_BUILD_TESTING=OFF", *std_cmake_args
+    system "make", "-C", "build-shared"
+    system "make", "-C", "build-shared", "install"
+  end
+
+  test do
+    (testpath/"test.cpp").write <<~EOS
+      #include <re2/re2.h>
+      #include <assert.h>
+      int main() {
+        assert(!RE2::FullMatch("hello", "e"));
+        assert(RE2::PartialMatch("hello", "e"));
+        return 0;
+      }
+    EOS
+    system ENV.cxx, "-std=c++11", "test.cpp", "-o", "test",
+                    "-I#{include}", "-L#{lib}", "-lre2"
+    system "./test"
+  end
+end
